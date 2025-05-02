7ee7331d654b4f72b43bf8841d5db310b6024698
mdiberna <mdiberna@fry.wi.mit.edu>
2025-05-02 15:59:28 -0400

    final AORTIC results

--- scripts/6.prepare_histobistro.py
+++ scripts/6.prepare_histobistro.py
@@ -9,5 +9,16 @@ clinical_df['isMSIH'] = clinical_df['isMSIH'].replace({
     'MSI-H': 'MSIH'
 })
 
-# Save the updated table
-clinical_df.to_csv("../tables/2/all_clinical_table_histobistro.csv", index=False)
+# Save the updated table as an excel file
+clinical_df.to_excel("../tables/2/all_clinical_table_histobistro.xlsx", index=False)
+
+# Load your current slide table
+slide_df = pd.read_csv("../tables/2/all_slide_table.csv")
+
+# For the FILENAME column, remove strip all the path text and keep only the file name
+slide_df['FILENAME'] = slide_df['FILENAME'].str.split('/').str[-1]
+# Remove the .svs extension from the FILENAME column
+slide_df['FILENAME'] = slide_df['FILENAME'].str.replace('.h5', '', regex=False)
+
+# Save the updated table as a csv file
+slide_df.to_csv("../tables/2/all_slide_table_histobistro.csv", index=False)
